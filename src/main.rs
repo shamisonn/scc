695b28e9bfaa07af392e618bfeb2f35c24472fcd
shamisonn <unknown0incident@gmail.com>
2019-11-29 21:03:03 +0900

    can use + - operators by bad coding

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,49 @@ use std::env;
 use std::process::exit;
 
 fn main() {
-    let first = match env::args().nth(1) {
-        Some(s) => s,
-        None => {
-            eprint!("[Error] args.get(1) is missing.");
-            exit(1);
-        }
-    };
+    let s = get_arg(1);
+    let mut chars = s.chars();
+
+    // print header
     println!(".intel_syntax noprefix");
     println!(".global _main");
     println!("_main:");
-    println!("  mov rax, {}", first);
+    print!("  mov rax, ");
+    loop {
+        let c = match chars.next() {
+            Some(c) => c,
+            None => {
+                println!();
+                break;
+            }
+        };
+        printByChar(c);
+    }
     println!("  ret");
+}
+
+fn get_arg(n: usize) -> String {
+    return match env::args().nth(n) {
+        Some(s) => s,
+        None => {
+            eprintln!("[Error] args().nth({}) is missing.", n);
+            exit(1);
+        }
+    };
+}
+
+fn printByChar(c: char) {
+    match c {
+        '+' => {
+            println!();
+            print!("  add rax, ");
+        }
+        '-' => {
+            println!();
+            print!("  sub rax, ");
+        }
+        c => {
+            print!("{}", c);
+        }
+    }
 }
